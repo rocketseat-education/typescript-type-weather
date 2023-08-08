dddf5df8e070609c9062269488df853dfe904331
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-08-08 01:35:49 -0300

    Criando o WeatherAPIResponseProps

--- src/services/getWeatherByCity.ts
+++ src/services/getWeatherByCity.ts
@@ -9,8 +9,30 @@ interface GetWeatherByCityProps {
   longitude: number;
 }
 
+export interface WeatherAPIResponseProps {
+  list: {
+    dt_txt: string;
+    pop: number;
+    main: {
+      temp: number;
+      temp_min: number;
+      temp_max: number;
+      feels_like: number;
+      humidity: number;
+      temp_kf: number;
+    },
+    wind: {
+      speed: number;
+    },
+    weather: {
+      main: string;
+      description: string;
+    }[];
+  }[];
+}
+
 export async function getWeatherByCity({ latitude, longitude }: GetWeatherByCityProps) {
-  const { data } = await api.get(`/forecast?lat=${latitude}&lon=${longitude}`);
+  const { data } = await api.get<WeatherAPIResponseProps>(`/forecast?lat=${latitude}&lon=${longitude}`);
   const { main, weather, wind, pop } = data.list[0];
 
   console.log(weather)
