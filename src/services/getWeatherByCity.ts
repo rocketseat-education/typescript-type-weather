ec18d854cddbb1fa6477db8045b35d5743ae7769
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-08-08 02:24:24 -0300

    Finalizando o getWeatherByCity e NextDaysItem

--- src/services/getWeatherByCity.ts
+++ src/services/getWeatherByCity.ts
@@ -3,6 +3,7 @@ import dayjs from "dayjs";
 import { api } from "./api";
 import { getNextDays } from "../utils/getNextDays";
 import { weatherIcons, WeatherIconsKeysProps } from "../utils/weatherIcons";
+import { NextDaysItemProps } from "../components/NextDaysItem";
 
 interface GetWeatherByCityProps {
   latitude: number;
@@ -55,8 +56,8 @@ export async function getWeatherByCity({ latitude, longitude }: GetWeatherByCity
   }
 
   const days = getNextDays();
-  const daysAdded = [];
-  const nextDays = [];
+  const daysAdded: string[] = [];
+  const nextDays: NextDaysItemProps[] = [];
 
   data.list.forEach((item) => {
     const day = dayjs(new Date(item.dt_txt)).format('DD/MM');
